88701b5d25bdc2612b515b9c125266b7b094b7d2
Annabel Cheng <chen1036@algonquinlive.com>
2025-05-22 14:12:23 -0400

    added 4 buttons within Column section.

---
+++ lib/Lab3.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Lab 3 : Flutter Demo by Annabel Cheng'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var myFontSize = 30.0;
+  late TextEditingController _controller1; // this is to read what is typed
+  late TextEditingController _controller2; // this is to read what is typed
+  late String password; // nothing yet, but not null
+  var imageSource = "images/question-mark.png";
+
+  void buttonClicked(){
+  }
+
+  @override
+  void initState() { // similar to onloaded= (in html)
+    super.initState();
+    _controller1 = TextEditingController(); //making _controller
+    _controller2 = TextEditingController(); //making _controller
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    super.dispose(); //free the memory of what was typed
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+              }, // onPressed
+              child:Text("Button 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+              }, // onPressed
+              child:Text("Button 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+              }, // onPressed
+              child:Text("Button 3"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+              }, // onPressed
+              child:Text("Button 4"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
