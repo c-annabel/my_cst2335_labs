96860dc2c5e1900e7e939d95fa54244fb13608ee
Annabel Cheng <chen1036@algonquinlive.com>
2025-05-22 21:47:06 -0400

    created 6 rows and 3 subtitles.

--- lib/Lab3.dart
+++ lib/Lab3.dart
@@ -44,30 +44,126 @@ class _MyHomePageState extends State<MyHomePage> {
              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(36.0),
-                    color: Colors.green[900],
-                    width: double.infinity,
-                    child: Text(
-                      widget.title,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 24, color: Colors.white,
-                             fontWeight: FontWeight.bold, letterSpacing: 2.0,),
-                    ),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10.0, 6.0, 8.0, 10.0),
-                    child:
-                      Text(
-                        "Not sure about exactly which recipe you're looking for? "
-                        "Do a search, or dive into our most popular categories.",
-                        // textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 15.0,
-                                         fontWeight: FontWeight.bold,)
+                    Container(
+                      padding: EdgeInsets.all(36.0),
+                      color: Colors.green[900],
+                      width: double.infinity,
+                      child: Text(
+                        widget.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 24, color: Colors.white,
+                               fontWeight: FontWeight.bold, letterSpacing: 2.0,),
                       ),
-                  ),
+                    ),
 
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 6.0, 8.0, 10.0),
+                      child:
+                        Text(
+                          "Not sure about exactly which recipe you're looking for? "
+                          "Do a search, or dive into our most popular categories.",
+                          // textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 15.0,
+                                           fontWeight: FontWeight.bold,)
+                        ),
+                    ),
+
+                    //----MAIN CATEGORIES START---------------------------------------------------//
+
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 6.0, 8.0, 10.0),
+                      color: Colors.grey[200],
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                               Expanded(
+                                 child: Center(
+                                   child:
+                                   Text(
+                                       "BY MEAT",
+                                       textAlign: TextAlign.center,
+                                       style: TextStyle(fontSize: 20.0,
+                                         fontWeight: FontWeight.bold,)
+                                   ),
+                                 )
+                               )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text('Row 1, Item 1'),
+                              Spacer(),
+                              Text('Row 1, Item 2'),
+                              Spacer(),
+                              Text('Row 1, Item 3'),
+                              Spacer(),
+                              Text('Row 1, Item 4'),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Center(
+                                    child:
+                                    Text(
+                                        "BY COURSE",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,)
+                                    ),
+                                  )
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text('Row 2, Item 1'),
+                              Spacer(),
+                              Text('Row 2, Item 2'),
+                              Spacer(),
+                              Text('Row 2, Item 3'),
+                              Spacer(),
+                              Text('Row 2, Item 4'),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Center(
+                                    child:
+                                    Text(
+                                        "BY DESSERT",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,)
+                                    ),
+                                  )
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text('Row 3, Item 1'),
+                              Spacer(),
+                              Text('Row 3, Item 2'),
+                              Spacer(),
+                              Text('Row 3, Item 3'),
+                              Spacer(),
+                              Text('Row 3, Item 4'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+
+
+                    //-----MAIN CATEGORIES END---------------------------------------------------//
                 ],
              ),
 
