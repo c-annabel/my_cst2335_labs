31a08673d977897ac54aabc1da8f88deb04b72f4
Annabel Cheng <chen1036@algonquinlive.com>
2025-06-06 06:10:16 -0400

    Created Lab5 from Lab4 for fresh start. added url_launcher. added OtherPage file from the lecture material.

---
+++ lib/OtherPage.dart
@@ -0,0 +1,15 @@
+
+
+
+class OtherPage extends StatefulWidget {
+  @override
+  State<OtherPage> createState() => OtherPageState();
+}
+
+class OtherPageState extends State<OtherPage>
+{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(appBar: , body:  ); //Use a Scaffold to layout a page with an AppBar and main body region
+  }
