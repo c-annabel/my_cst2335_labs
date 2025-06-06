90b4dc631ae4420f27cb725c314e890a25b3a6a0
Annabel Cheng <chen1036@algonquinlive.com>
2025-06-06 04:29:43 -0400

    Restore to original Lab2 Code. Restart constructing.

--- lib/Lab4.dart
+++ lib/Lab4.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Lab 4 : Flutter Demo by Annabel Cheng'),
+      home: const MyHomePage(title: 'Lab 2 : Flutter Demo by Annabel Cheng'),
     );
   }
 }
@@ -36,6 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController _controller1; // this is to read what is typed
   late TextEditingController _controller2; // this is to read what is typed
   late String password; // nothing yet, but not null
+  var imageSource = "images/question-mark.png";
 
   void buttonClicked(){
   }
@@ -45,75 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _controller1 = TextEditingController(); //making _controller
     _controller2 = TextEditingController(); //making _controller
-
-    loadEncryptedLogin();
-  }
-
-  void loadEncryptedLogin() {
-    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-    prefs.getString("LoginName").then((String? loginName) {
-      prefs.getString("Password").then((String? passwordValue) {
-        if (loginName != null && passwordValue != null) {
-          setState(() {
-            _controller1.text = loginName;
-            _controller2.text = passwordValue;
-          });
-
-          // Show SnackBar to say data was loaded
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text("Previous login loaded!")),
-            );
-          });
-        }
-      });
-    });
-  }
-
-  void saveEncryptedLogin() {
-    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-    prefs.setString("LoginName", _controller1.text).then((bool success) {
-      prefs.setString("Password", _controller2.text).then((bool success2) {
-        print("Encrypted login saved.");
-      });
-    });
-  }
-
-  void clearEncryptedLogin() {
-    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-    prefs.remove("LoginName").then((bool success) {
-      prefs.remove("Password").then((bool success2) {
-        print("Encrypted login cleared.");
-      });
-    });
-  }
-
-  void showSaveDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Save Login?"),
-          content: Text("Would you like to save your username and password?"),
-          actions: [
-            TextButton(
-              child: Text("Yes"),
-              onPressed: () {
-                saveEncryptedLogin();
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("No"),
-              onPressed: () {
-                clearEncryptedLogin();
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
@@ -132,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(controller: _controller1,
                 decoration: InputDecoration(
@@ -149,20 +79,25 @@ class _MyHomePageState extends State<MyHomePage> {
                     labelText: "Password"
                 )
             ),
-
             ElevatedButton(
-                onPressed: () {
-
-                  // get the string that was typed in the password field
-                   password = _controller2.text;// <--- lambda function
-                   setState(() {
-                       // Show dialog asking to save login info
-                       showSaveDialog();
-                   });
-                }, // onPressed
-                child:Text("Login", style: TextStyle(fontSize:myFontSize, color:Colors.lightBlue)),
+              onPressed: () {
+                // get the string that was typed in the password field
+                password = _controller2.text;// <--- lambda function
+                setState(() {
+                  if (password == 'QWERTY123') {
+                    imageSource = "images/idea.png";
+                  }
+                  else {
+                    imageSource = "images/stop.png";
+                  }
+                });
+              }, // onPressed
+              child:Text("Login", style: TextStyle(fontSize:myFontSize, color:Colors.lightBlue)),
             ),
-
+            Semantics (
+                label:imageSource,
+                child: Image.asset(imageSource, width: 200, height:200)
+            ),
           ],
         ),
       ),
